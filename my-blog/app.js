4211a743f95c30eb84db70db8fd0e5142c5ddc25
za-xielingjuan <za-xielingjuan@zhongan.io>
2018-08-23 18:10:22 +0800

    feat: 加入博客身份验证、显示创建时间浏览数、删除

--- my-blog/app.js
+++ my-blog/app.js
@@ -3,10 +3,26 @@ const Route = require('koa-router');
 const render = require('./lib/render');
 const logger = require('koa-logger');
 const koaBody = require('koa-body');
+const auth = require('koa-basic-auth');
 const app = new Koa();
 const router = new Route();
 
-const posts = [];
+let posts = [];
+
+app.use(async function(ctx, next) {
+  try {
+    await next();
+  } catch (err) {
+    if (err.status === 401) {
+      ctx.status = 401;
+      ctx.set('WWW-Authenticate', 'Basic');
+      ctx.body = 'cannot haz that';
+    } else {
+      throw err;
+    }
+  }
+});
+app.use(auth({ name: 'xlj', pass: 'xlj' }));
 
 app.use(render);
 app.use(logger());
@@ -23,23 +39,31 @@ async function add(ctx) {
 async function show(ctx) {
   const id = ctx.params.id;
   const post = posts[id];
+  posts[id].views++;
+  console.log('post...', posts);
   await ctx.render('show', {post: post});
 };
 
 async function create(ctx) {
   const post = ctx.request.body;
-  console.log('post...', post);
   const id = posts.push(post) - 1;
   post.id = id;
   post.created_at = new Date();
-  console.log('posts...', posts);
+  post.views = 0; // 浏览数
   ctx.redirect('/');
 };
+
+async function deleteBlog(ctx) {
+  const id = ctx.params.id;
+  posts.splice(id, 1);
+  ctx.redirect('/');
+}
 
 router.get('/', list)
    .get('/post/new', add)
    .get('/post/:id', show)
-   .post('/post', create);
+   .post('/post', create)
+   .get('/delete/:id', deleteBlog);
 
 app.use(router.routes());
 app.listen(3000);
